03cc884174d4460f69c2719bd1ea81c05a4d62ea
Tyler Norbury <tylernorbury96@gmail.com>
2021-01-28 10:29:36 -0800

    add route guards

--- src/router.js
+++ src/router.js
@@ -5,9 +5,10 @@ import CoachDetails from './pages/CoachDetails.vue';
 import CoachRegistration from './pages/CoachRegistration.vue';
 import RequestsReceived from './pages/RequestsReceived.vue';
 import ContactCoach from './pages/ContactCoach.vue';
-import UserAuth from "./pages/auth/UserAuth.vue"
-
+import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
+
+import store from './store/index.js';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -26,11 +27,39 @@ const router = createRouter({
         }
       ]
     },
-    { path: "/auth", component: UserAuth },
-    { path: '/register', component: CoachRegistration },
-    { path: '/requests', component: RequestsReceived },
+    { path: '/auth', component: UserAuth, meta: { requiresUnAuth: true } },
+    {
+      path: '/register',
+      component: CoachRegistration,
+      meta: { requiresAuth: true, noCoach: true }
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+      meta: { requiresAuth: true }
+    },
     { path: '/:notFound(.*)', component: NotFound }
   ]
+});
+
+router.beforeEach(function(to, from, next) {
+  // If route requires authentication, and we're not authenticated, redirect
+  // to the auth page.
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth?mustAuth');
+  }
+  // If we're authenticated, and the routes requires us to not be authenticated
+  // OR requires us to not be a coach, then we'll redirect to the coaches page
+  else if (
+    store.getters.isAuthenticated &&
+    (to.meta.requiresUnAuth || (to.meta.noCoach && store.getters.isCoach))
+  ) {
+    next('/coaches');
+  }
+  // Otherwise, let 'em pass
+  else {
+    next();
+  }
 });
 
 export default router;
